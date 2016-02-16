62722bfe0600e91777d1005ac682ef960e203fcd
suragnair <suragnair@hotmail.com>
2016-02-17 02:54:10 +0530

    networking ready

--- MoodleApp/app/src/main/java/io/github/suragnair/moodleapp/MainActivity.java
+++ MoodleApp/app/src/main/java/io/github/suragnair/moodleapp/MainActivity.java
@@ -1,7 +1,11 @@
 package io.github.suragnair.moodleapp;
 
+import android.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import io.github.suragnair.moodleapp.Networking;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Networking.getData(0, new String[]{"cs1110200", "john"}, new Networking.VolleyCallback() {
+            @Override
+            public void onSuccess(String result) {
+                Networking.getData(2, new String[0], new Networking.VolleyCallback() {
+                    @Override
+                    public void onSuccess(String result) {
+                        alertMessageBox("lulz", result);
+                    }
+                });
+            }
+        });
+
+
+    }
+
+
+    private void alertMessageBox (String title, String message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(title);
+        builder.setMessage(message);
+        builder.setNeutralButton("OKAY", null);
+        builder.show();
     }
 }
--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 An android app for Moodle.
 
+# References
+* http://stackoverflow.com/questions/28120029/how-can-i-return-value-from-function-onresponse-of-volley
+* http://stackoverflow.com/questions/16680701/using-cookies-with-android-volley-library
+* http://developer.android.com/training/volley/simple.html
